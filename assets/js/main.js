df6cf1aaf93f5699b48c9cf89e7fcc0b408bde3c
Hettaa <mohamed.hatem.me@gmail.com>
2021-04-19 17:20:54 +0200

    edit ra project

--- assets/js/main.js
+++ assets/js/main.js
@@ -304,6 +304,40 @@ $(document).ready(function() {
             }
         ]
     });
+    /*best seller slider*/
+    $('.best-seller-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        swipeToSlide: true,
+        arrows: true,
+
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 
     /*inner product slider*/
     $('.inner-product-slider').slick({
@@ -313,41 +347,6 @@ $(document).ready(function() {
         arrows: false,
     });
 
-
-    //best seller slider
-    $('.best-seller-slider').slick({
-        dots: false,
-        infinite: true,
-        autoplay: false,
-        autoplaySpeed: 4000,
-        // rtl: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
 
     //flex slider
 
@@ -405,6 +404,113 @@ $(document).ready(function() {
                 }
             }
         ],
+    });
+
+    /*new arrival slider*/
+    $('.new-arrival-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        swipeToSlide: true,
+        arrows: true,
+
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    /*trendy now slider*/
+    $('.trendy-now-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        swipeToSlide: true,
+        arrows: true,
+
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    //shopby-collection slider
+    $('.shopby-collection-slider').slick({
+        dots: true,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        speed: 2000,
+        variableWidth: true,
+        slidesToShow: 5,
+        slidesToScroll: 4,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: false,
+                }
+            }
+        ]
     });
 
     // range slider
